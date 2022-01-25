3f00ef11f5bc5e54970f34668f5d77c7152c52ba
= <=>
2022-01-25 17:47:41 +0530

    using useLogout hook in navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
+import { useLogout } from "../../hooks/useLogout";
 
 // styles
 import styles from "./Navbar.module.css";
 
 export default function Navbar() {
+  const { logout } = useLogout();
   return (
     <nav className={styles.navbar}>
       <ul>
@@ -16,6 +18,11 @@ export default function Navbar() {
         </li>
         <li>
           <Link to="/signup">Signup</Link>
+        </li>
+        <li>
+          <button className="btn" onClick={logout}>
+            Logout
+          </button>
         </li>
       </ul>
     </nav>
