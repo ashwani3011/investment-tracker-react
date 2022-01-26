77d439483966d6c2f98246daf4bf834e2072dc70
= <=>
2022-01-26 10:46:53 +0530

    listing investment

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,15 +1,21 @@
 import { useAuthContext } from "../../hooks/useAuthContext";
+import { useCollection } from "../../hooks/useCollection";
 // styles
 import styles from "./Home.module.css";
 
 // components
 import TransactionForm from "./TransactionForm";
+import TransactionList from "./TransactionList";
 
 export default function Home() {
   const { user } = useAuthContext();
+  const { documents, error } = useCollection("transaction");
   return (
     <div className={styles.container}>
-      <div className={styles.content}>transaction list</div>
+      <div className={styles.content}>
+        {error && <p>{error}</p>}
+        {documents && <TransactionList transactions={documents} />}
+      </div>
       <div className={styles.sidebar}>
         <TransactionForm uid={user.uid} />
       </div>
