16d832a1a2a95dc30562b9013f4d9e9ad810765d
= <=>
2022-01-26 09:22:53 +0530

    firestore timestam

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -18,4 +18,7 @@ firebase.initializeApp(firebaseConfig);
 const projectFirestore = firebase.firestore();
 const projectAuth = firebase.auth();
 
+// timestamp
+const timestamp = firebase.firestore.Timestamp;
+
 export { projectFirestore, projectAuth };
--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -1,5 +1,5 @@
 import { useReducer, useEffect, useState } from "react";
-import { projectFirestore } from "../firebase/config";
+import { projectFirestore, timestamp } from "../firebase/config";
 
 let initialState = {
   document: null,
@@ -50,8 +50,8 @@ export const useFirestore = (collection) => {
     dispatch({ type: "IS_PENDING" });
 
     try {
-      const addedDocument = await ref.add({ ...doc });
-      dispatchIfNotCancelled({
+        const createdAt = timestamp.fromDate(new Date())
+        const addedDocument = await ref.add({...doc, createdAt })dispatchIfNotCancelled({
         type: "ADDED_DOCUMENT",
         payload: addedDocument,
       });
