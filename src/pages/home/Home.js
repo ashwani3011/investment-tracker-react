d29d222ad7bd8bd0655eacad9c8ada6e9bf972f7
= <=>
2022-01-26 10:55:02 +0530

    firestore queries (where) used to show document of logged in user only

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -9,7 +9,11 @@ import TransactionList from "./TransactionList";
 
 export default function Home() {
   const { user } = useAuthContext();
-  const { documents, error } = useCollection("transaction");
+  const { documents, error } = useCollection("transaction", [
+    "uid",
+    "==",
+    user.uid,
+  ]);
   return (
     <div className={styles.container}>
       <div className={styles.content}>
